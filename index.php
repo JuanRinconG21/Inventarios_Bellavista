f703526de70464ba780fb14200468d0c75e6193e
camila-Lozano-90 <mclozano90@misena.edu.co>
2024-02-02 09:48:16 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventarios Bellavista</title>
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+</head>
+<body>
+<div class="wrapper">
+        <div class="logo">
+            <img src="./img/Logo_Bellavista.jpg.jpeg" alt="">
+        </div>
+        <div class="text-center mt-4 name">
+            Bienvenido
+        </div>
+        <div class="text-center mt-4 name">
+            Sistema De Control Para El Almacen 
+        </div>
+        <form action="" method="" class="p-3 mt-3">
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="cedula" id="userName" placeholder="Cedula">
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input type="password" name="password" id="pwd" placeholder="Contraseña">
+            </div>
+            <button class="btn mt-3">Ingresar</button>
+        </form>
+    </div>
+</body>
+</html>
