6aa873e6aa180f7bc3fea49fd08d13d1ac72c3fe
Jota <jotacodigos@gmail.com>
2024-02-02 09:52:42 -0500

    second commit

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+    
 </head>
 <body>
 <div class="wrapper">
